b682f1b9209cf3738fbc1897c7aabeecc522e538
FunmilayoUde <137394778+FunmilayoUde@users.noreply.github.com>
2024-02-07 13:47:23 +0100

    Update settings.py

--- colortrans/settings.py
+++ colortrans/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY =  'django-insecure-64k3ff!!&q&7c^8c$%u5zmp0_^k2uv58s)fuc490d5)tv^+$l2'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
